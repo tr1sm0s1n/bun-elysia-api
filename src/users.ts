a4eb7e23f3647d6f54bf41544a03f8d00860a6f8
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-07-26 22:14:17 +0530

    users_added

---
+++ src/users.ts
@@ -0,0 +1,30 @@
+export const users = [
+  {
+    id: "p-01",
+    name: "Corliss",
+    age: 28,
+    origin: "Europa",
+    admin: false,
+  },
+  {
+    id: "p-02",
+    name: "Jared",
+    age: 45,
+    origin: "Earth",
+    admin: true,
+  },
+  {
+    id: "p-03",
+    name: "Raphael",
+    age: 32,
+    origin: "Mars",
+    admin: false,
+  },
+  {
+    id: "p-04",
+    name: "Zoe",
+    age: 24,
+    origin: "Enceladus",
+    admin: false,
+  },
+];
