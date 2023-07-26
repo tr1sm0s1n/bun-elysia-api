ef1822ce30e8c62fe43a0b7b0c4374bcec61de99
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-07-26 22:19:55 +0530

    routes_updated

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,81 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { users } from "./users";
+
+const findOne = (id: string) => {
+  return users.find((user) => {
+    return user.id === id;
+  });
+};
 
 const app = new Elysia()
-  .get("/read", () => "Fetched all users")
-  .get("/read/:id", ({ params: { id } }) => `Fetched user ${id}`)
-  .post("/create", () => "Created a user")
-  .put("/update", () => "Updated a user")
-  .delete("/delete", () => "Deleted a user")
-  .listen(3000);
+  .get("/read", () => users)
+  .get("/read/:id", ({ params: { id }, set }) => {
+    let user = findOne(id);
+    if (user) {
+      return user;
+    } else {
+      set.status = 404;
+      return "Not found";
+    }
+  })
+  .post(
+    "/create",
+    ({ body, set }) => {
+      let existing = findOne(body.id);
+      if (existing) {
+        set.status = 400;
+        return "User already exists";
+      } else {
+        users.push(body);
+        set.status = 201;
+        return body;
+      }
+    },
+    {
+      body: t.Object({
+        id: t.String(),
+        name: t.String(),
+        age: t.Number(),
+        origin: t.String(),
+        admin: t.Boolean(),
+      }),
+    }
+  )
+  .put(
+    "/update",
+    ({ body, set }) => {
+      let existing = findOne(body.id);
+      if (existing) {
+        let updated = Object.assign(existing, body);
+        return updated;
+      } else {
+        users.push(body);
+        set.status = 201;
+        return body;
+      }
+    },
+    {
+      body: t.Object({
+        id: t.String(),
+        name: t.String(),
+        age: t.Number(),
+        origin: t.String(),
+        admin: t.Boolean(),
+      }),
+    }
+  )
+  .delete("/delete/:id", ({ params: { id }, set }) => {
+    let existing = findOne(id);
+    if (existing) {
+      let index = users.findIndex((user) => user.id === existing?.id);
+      users.splice(index, 1);
+      return existing;
+    } else {
+      set.status = 404;
+      return "Not found";
+    }
+  })
+  .listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
